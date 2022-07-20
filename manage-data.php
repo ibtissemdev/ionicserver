dced247b29a2a3a3b857b92194eaf71f7465559b
ibtissemdev <ibtissem.khiri@gmail.com>
2022-07-20 10:39:24 +0200

    création des pages

--- manage-data.php
+++ manage-data.php
@@ -1,14 +1,37 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type');
-
 // TODO: Définir les paramètres de connexion à la base
+class Database {
+  // Connexion à la base de données
+  private $host = "localhost";
+  private  $db_name = "ionicfoundlost";
+  private  $username = "root";
+  private  $password = "";
+  protected  $pdo;
+ 
+  public function getPdo()
+  {
+    // TODO: Créer une instance de la classe PDO
+          $this->pdo = new PDO("mysql:host=" . $this->host.";dbname=".$this->db_name, $this->username, $this->password, [
+             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8',
+             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+         ]);
+        
+ return $this->pdo;
+  } 
+     
+ }
+print_r($_GET);
 
-// TODO: Créer une instance de la classe PDO
 
 // Récupérer le paramètre d’action de l’URL du client depuis $_GET[‘key’] 
 // et nettoyer la valeur
-$key = strip_tags($_GET['key']);
+
+$key = strip_tags($_GET['action']);
+print_r($key);
+
 
 // Récupérer les paramètres envoyés par le client vers l’API
 $input = file_get_contents('php://input');
@@ -29,7 +52,36 @@ if (!empty($input)) {
   //Ajoute un nouvel enregistrement
  case "create":
 //   TODO: Filtrer les valeurs entrantes
+
+
 //   TODO: Préparer la requête dans un try/catch
+try {
+
+  //   Ajout nouvelle entrée
+  
+      $objet = $this->pdo->prepare("INSERT INTO foundlost (status,description, date, location, firstname, lastname,email)
+      VALUES(:status, :description, :date, :location, :firstname, :lastname, :email)");
+  
+      $objet->bindParam(':status',$status);
+      $objet->bindParam(':description',$description);
+      $objet->bindParam(':date',$date);
+      $objet->bindParam(':location',$location);
+      $objet->bindParam(':firstname',$firstname);
+      $objet->bindParam(':lastname',$lastname);
+      $objet->bindParam(':email',$email);
+      
+      $objet->execute();
+  
+      
+  }
+  
+  catch(PDOException $e){
+      echo 'Impossible de traiter les données. Erreur : '.$e->getMessage();
+  }
+  
+
+
+
 break;
 
   // Mettre à jour un enregistrement existant
